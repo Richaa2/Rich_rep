dcf5e5f53bdb39ad407a8692617170ba2bfcbef9
Richard <richarddkochut@gmail.com>
2022-03-04 03:22:46 +0200

    1

---
+++ Navigators/navigator2/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:navigator2/screen/first_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Returned Pass Data',
+    home: FirstScreen(),
+  ));
+}
