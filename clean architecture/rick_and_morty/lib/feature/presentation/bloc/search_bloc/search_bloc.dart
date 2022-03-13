e2ffa36d7d2e0179795517680ccedef8243a6e30
Richard <richarddkochut@gmail.com>
2022-03-13 22:51:49 +0200

    bloc

---
+++ clean architecture/rick_and_morty/lib/feature/presentation/bloc/search_bloc/search_bloc.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/core/error/failure.dart';
+import 'package:rick_and_morty/feature/domain/usecases/search_person.dart';
+
+import 'package:rick_and_morty/feature/presentation/bloc/search_bloc/search_event.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/search_bloc/search_state.dart';
+
+const SERVER_FAILURE_MESSAGE = 'Server Failure';
+const CACHED_FAILURE_MESSAGE = 'Cache Failure';
+
+class PersonSearchBloc extends Bloc<PersonSearchEvent, PersonSearchState> {
+  final SearchPerson searchPerson;
+
+  PersonSearchBloc({required this.searchPerson}) : super(PersonEmpry()) {
+    on<SearchPersons>(_onEvent);
+  }
+  FutureOr<void> _onEvent(
+      SearchPersons event, Emitter<PersonSearchState> emit) async {
+    emit(PersonSearchLoading());
+    final failureOrPerson =
+        await searchPerson(SearchPersonParams(query: event.personQuery));
+    emit(failureOrPerson.fold(
+        (failure) => PersonSearchError(message: _mapFailureToMessage(failure)),
+        (person) => PersonSearchLoaded(persons: person)));
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHED_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected Error';
+    }
+
+    //   on<PersonSearchLoading>(event, emit) async {
+    //     try {
+    //       final _loadedSearchPersonList = await searchPerson();
+    //       emit(PersonSearchLoaded(persons: _loadedSearchPersonList));
+    //     } catch (_) {
+    //       PersonSearchError(message: 'Error PersonSearchLoading');
+    //     }
+    //   }
+    // }
+  }
+}
