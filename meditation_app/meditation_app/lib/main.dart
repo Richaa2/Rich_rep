a3a6e008a04722ce388fd8e09c7dfb589294a2b7
Richard <richarddkochut@gmail.com>
2022-04-13 01:25:23 +0300

    Update main.dart

--- meditation_app/meditation_app/lib/main.dart
+++ meditation_app/meditation_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meditation_app/screens/meditation_app_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
+      home: MeditationAppScreen( ),
     );
   }
 }
